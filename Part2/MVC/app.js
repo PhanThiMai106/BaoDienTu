1cd44eddbafbfafe707ea226270c9b929d2d507c
hoangnhi1003 <31733796+hoangnhi1003@users.noreply.github.com>
2019-06-15 12:31:04 +0700

    Add files via upload

--- Part2/MVC/app.js
+++ Part2/MVC/app.js
@@ -13,9 +13,13 @@ app.use(express.urlencoded());
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
  
+app.use(require('./middlewares/local.mdw'));
+
 app.get('/', function (req, res) {
     res.render('home');
 });
+
+app.use('/admin/categories', require('./routes/category.route'));
 
 app.use('/admin/categories', require('./routes/admin/category.route'));
 
@@ -24,5 +28,13 @@ app.use('/admin/myaccount', require('./routes/admin/account.route'));
 app.use('/admin/users', require('./routes/admin/user.route'));
 
 app.use('/admin/hashtag', require('./routes/admin/tag.route'));
+
+app.use('/admin/Articles', require('./routes/admin/article.route'));
+
+app.use('/writer/post', require('./routes/writer/post.route'))
+
+app.use('/editer/index', require('./routes/editer/article.route'))
+
+app.use('/subcriber/account', require('./routes/subcriber/account.rout'))
 
 app.listen(3000);
